edefd8ffa0971ab954a03817ee8f14c91800b0b2
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-09-19 12:26:48 +0200

    perfect !

--- src/todo.py
+++ src/todo.py
@@ -1,15 +1,62 @@
 import sqlite3
-from bottle import Bottle
+from bottle import Bottle, template, request
 
 app = Bottle()
 
-@app.route('/todo')
+@app.get('/my_todo_list')
+@app.get('/todo')
 def todo_list():
+    show  = request.query.show or 'open'
+    match show:
+        case 'open':
+            db_query = "SELECT id, task FROM todo WHERE status LIKE '1'"
+        case 'closed':
+            db_query = "SELECT id, task FROM todo WHERE status LIKE '0'"
+        case 'all':
+            db_query = "SELECT id, task FROM todo"
+        case _:
+            return template('message.tpl',
+                message = 'Wrong query parameter: show must be either open, closed or all.')
     with sqlite3.connect('todo.db') as connection:
         cursor = connection.cursor()
-        cursor.execute("SELECT id, task, status FROM todo WHERE status LIKE '1'")
+        cursor.execute(db_query)
         result = cursor.fetchall()
-        return str(result)
+    output = template('show_tasks.tpl', rows=result)
+    return output
+
+@app.route('/new', method=['GET', 'POST'])
+def new_task():
+    if request.POST:
+        new_task = request.forms.task.strip()
+        with sqlite3.connect('todo.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new_task, 1))
+            new_id = cursor.lastrowid
+        return template('message.tpl',
+            message=f'The new task was inserted into the database, the ID is {new_id}')
+    else:
+        return template('new_task.tpl')
+
+@app.route('/edit/<number:int>', method=['GET', 'POST'])
+def edit_task(number):
+    if request.POST:
+        new_data = request.forms.task.strip()
+        status = request.forms.status.strip()
+        if status == 'open':
+            status = 1
+        else:
+            status = 0
+        with sqlite3.connect('todo.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute("UPDATE todo SET task = ?, status = ? WHERE id LIKE ?", (new_data, status, number))
+        return template('message.tpl',
+            message=f'The task number {number} was successfully updated')
+    else:
+        with sqlite3.connect('todo.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT task FROM todo WHERE id LIKE ?", (number,))
+            current_data = cursor.fetchone()
+        return template('edit_task', current_data=current_data, number=number)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8081)
+    app.run(host='localhost', port=8080, debug=True, reloader=True)
---
+++ src/views/message.tpl
@@ -0,0 +1,3 @@
+%rebase('base.tpl')
+<h1>{{message}}</h1>
+<p><a href="/todo">Back to main page</p>
