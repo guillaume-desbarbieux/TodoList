fe2fdbd9da3997c5e249b4015e1d42fc4b1dfee9
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-09-19 11:15:37 +0200

    Route Todo Creation DB

---
+++ src/todo.py
@@ -0,0 +1,15 @@
+import sqlite3
+from bottle import Bottle
+
+app = Bottle()
+
+@app.route('/todo')
+def todo_list():
+    with sqlite3.connect('todo.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT id, task, status FROM todo WHERE status LIKE '1'")
+        result = cursor.fetchall()
+        return str(result)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8081)
